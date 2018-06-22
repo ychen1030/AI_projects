e929251ebcfdd3ec740c759a340c0c9b32f065be
Yingying Chen <yingying@berkeley.edu>
2018-06-22 13:47:04 -0700

    finish project

--- search/util.py
+++ search/util.py
@@ -227,6 +227,7 @@ def manhattanDistance( xy1, xy2 ):
     "Returns the Manhattan distance between points xy1 and xy2"
     return abs( xy1[0] - xy2[0] ) + abs( xy1[1] - xy2[1] )
 
+
 """
   Data structures and functions useful for various course projects
 
